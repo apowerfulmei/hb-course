63005a31bb30375df1321c8a803c8a2d793a268b
apowerfulmei <528320434@qq.com>
2024-04-17 16:34:11 +0800

    删除了多余文件

--- upload-labs/Pass-05/helper.php
+++
@@ -1,5 +0,0 @@
-<?php
-if($_GET['action'] == 'get_prompt'){
-    echo '上传目录存在php文件（readme.php）';
-}
-?>
--- upload-labs/Pass-21/helper.php
+++
@@ -1,5 +0,0 @@
-<?php
-if($_GET['action'] == 'get_prompt'){
-    echo 'Pass-20来源于CTF，请审计代码！';
-}
-?>
